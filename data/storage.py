f277aa8380637847907e45651ac5edd3e3fdb5fc
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-05-18 15:40:57 +0430

    data/storage.py file

---
+++ data/storage.py
@@ -0,0 +1,11 @@
+from models.product import Product
+
+products = [
+    Product(1, "Laptop", 10000, 5, "Electronics"),
+    Product(2, "Phone", 8500, 10, "Mobile"),
+    Product(3, "Keyboard", 760, 20, "Accessories"),
+    Product(4, "Mouse", 450, 30, "Accessories"),
+    Product(5, "Monitor", 3200, 8, "Electronics"),
+    Product(6, "Headphone", 1200, 15, "Gaming"),
+    Product(7, "Smart Watch", 2500, 12, "Mobile"),
+]
