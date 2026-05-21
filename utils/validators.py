acf1eb3ddb0e8b690aa79da34c01fb66d1638120
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-05-21 22:10:23 +0430

    utils/ validtors.py file

--- utils/validators.py
+++ utils/validators.py
@@ -1,16 +1,38 @@
 def validate_email(email):
+
     if "@" not in email or "." not in email:
         return False
+
+    if (
+        email.startswith('@') or
+        email.startswith('.') or
+        email.endswith('@') or
+        email.endswith('.')
+    ):
+        return False
+
     return True
 
 
 def validate_password(password):
+
     if len(password) < 8:
         return False
+
+    if not any(char.isdigit() for char in password):
+        return False
+
     return True
 
 
 def validate_name(name):
-    if len(name.strip()) < 2:
+
+    name = name.strip()
+
+    if len(name) < 2:
         return False
+
+    if any(char.isdigit() for char in name):
+        return False
+
     return True
